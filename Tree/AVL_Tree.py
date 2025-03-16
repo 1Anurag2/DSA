ebcaa4ff5bce10007ae103f339283128b9bb41a1
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-03-16 16:37:56 +0530

    AVL_Tree

---
+++ Tree/AVL_Tree.py
@@ -0,0 +1,85 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 1
+
+def getHeight(node):
+    if not node:
+        return 0
+    return node.height
+
+def getBalance(node):
+    if not node:
+        return 0
+    return getHeight(node.left) - getHeight(node.right)
+
+def rightRotate(y):
+    print('Rotate right on node',y.data)
+    x = y.left
+    T2 = x.right
+    x.right = y
+    y.left = T2
+    y.height = 1 + max(getHeight(y.left), getHeight(y.right))
+    x.height = 1 + max(getHeight(x.left), getHeight(x.right))
+    return x
+
+def leftRotate(x):
+    print('Rotate left on node',x.data)
+    y = x.right
+    T2 = y.left
+    y.left = x
+    x.right = T2
+    x.height = 1 + max(getHeight(x.left), getHeight(x.right))
+    y.height = 1 + max(getHeight(y.left), getHeight(y.right))
+    return y
+
+def insert(node, data):
+    if not node:
+        return TreeNode(data)
+
+    if data < node.data:
+        node.left = insert(node.left, data)
+    elif data > node.data:
+        node.right = insert(node.right, data)
+
+    # Update the balance factor and balance the tree
+    node.height = 1 + max(getHeight(node.left), getHeight(node.right))
+    balance = getBalance(node)
+
+    # Balancing the tree
+    # Left Left
+    if balance > 1 and getBalance(node.left) >= 0:
+        return rightRotate(node)
+
+    # Left Right
+    if balance > 1 and getBalance(node.left) < 0:
+        node.left = leftRotate(node.left)
+        return rightRotate(node)
+
+    # Right Right
+    if balance < -1 and getBalance(node.right) <= 0:
+        return leftRotate(node)
+
+    # Right Left
+    if balance < -1 and getBalance(node.right) > 0:
+        node.right = rightRotate(node.right)
+        return leftRotate(node)
+
+    return node
+
+def inOrderTraversal(node):
+    if node is None:
+        return
+    inOrderTraversal(node.left)
+    print(node.data, end=", ")
+    inOrderTraversal(node.right)
+
+# Inserting nodes
+root = None
+letters = ['C', 'B', 'E', 'A', 'D', 'H', 'G', 'F' ,'P' ,'I','Z','X','Y']
+for letter in letters:
+    root = insert(root, letter)
+
+inOrderTraversal(root)
