72cf403c90a80f0ba081aeb901a06590c21502ff
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-04-21 02:14:34 +0530

    Bubble_sort

--- sorting/Bubble_sort.cpp
+++ sorting/Bubble_sort.cpp
@@ -1,57 +1,29 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n,temp;
-    cout << "Enter the size of array : ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter elements of array : ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    } 
-    cout<<endl;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                temp = arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]= temp;
+
+void bubble_sort(int arr[], int k) {
+    int temp;
+    for (int i = 0; i < k - 1; i++) {            
+        for (int j = 0; j < k - i - 1; j++) {    
+            if (arr[j] > arr[j + 1]) {           
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    cout<<"Sorted array : ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
 }
 
-
-
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n,temp;
-//     cout<<"Enter the size of array : ";
-//     cin>>n;
-//     int arr[n];
-//     cout<<"Enter the array elements : ";
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     for(int i= n;i>0;i--){
-//         for(int j=0;j<i+1;j++){
-//             if(arr[j]>arr[j+1]){
-//                 temp = arr[j];
-//                 arr[j]=arr[j+1];
-//                 arr[j+1]=temp;
-//             }
-//         }
-//     }
-//     cout<<"Sorted Array : ";
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i]<<" ";
-//     }
-//     return 0;
-// }
+int main() {
+    int arr[] = {2, 6, 7, 4, 32, 1};   
+    int size = sizeof(arr) / sizeof(arr[0]); 
+    
+    bubble_sort(arr, size); 
+    
+    cout << "Sorted Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";  
+    }
+    cout << endl;
+    return 0;
+}
