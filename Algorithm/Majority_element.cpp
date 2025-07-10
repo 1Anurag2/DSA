5ec26e8b52f968f651323b357e1fe6fecea0a6c5
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-10 11:57:27 +0530

    Majority Element in an Array

--- Algorithm/Majority_element.cpp
+++ Algorithm/Majority_element.cpp
@@ -1,30 +1,50 @@
-//Optimal solution   
+//Moore's voting algorithm
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int freq = 1 , ans = nums[0];
-        for(int i = 1; i<n ; i++){
-            if(nums[i] == nums[i-1]){
-                freq++;
-            }else{
-                freq = 1;
-                ans = nums[i];
-            }
-        if(freq > n/2){
-            return ans;
+       int freq = 0 , ans = 0;
+       for(int i = 0 ; i<n;i++){
+        if(freq == 0){
+            ans = nums[i];
         }
+        if(ans == nums[i]){
+            freq++;
         }
-        return ans;
+        else{
+            freq--;
+        }
+
+       }
+       return ans;
     }
 };
 
 
+//optimal solution
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         int freq = 1 , ans = nums[0];
+//         for(int i = 1; i<n ; i++){
+//             if(nums[i] == nums[i-1]){
+//                 freq++;
+//             }else{
+//                 freq = 1;
+//                 ans = nums[i];
+//             }
+//         if(freq > n/2){
+//             return ans;
+//         }
+//         }
+//         return ans;
+//     }
+// };
 
 
-
-//Bruteforce approch time complexity = O(n^2)
 
 // class Solution {
 // public:
@@ -44,4 +64,3 @@ public:
 //         return -1;
 //     }
 // };
-
