8ed43b71e03d86f23db3a91ca9cc4f058c6089b2
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-26 23:15:47 +0530

    N Queens Problem .51

---
+++ Recursion/nQueens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+   bool isSafe(vector<string>&board,int row , int col, int n){
+        //horizontal
+        for(int j = 0 ; j<n;j++){
+            if(board[row][j] == 'Q'){
+                return false;
+            }
+        }
+
+        //vertical
+        for(int i = 0 ; i<n;i++){
+            if (board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left digonal
+        for(int i = row , j = col ; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row , j = col ; i>=0 && j>=0; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    void nQueens(vector<string>& board, int row, int n, vector<vector<string>>& ans) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board, row, j, n)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1, n, ans);
+                board[row][j] = '.'; // backtrack
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> ans;
+        nQueens(board, 0, n, ans);
+        return ans;
+    }
+};
