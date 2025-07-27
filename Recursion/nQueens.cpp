b6dbca2e121287d3c1250ed851dd6d116c2c6b23
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-27 14:16:55 +0530

    N Queens Problem Solution // This code solves the N Queens problem using backtracking. // It places N queens on an N x N chessboard such that no two queens threaten each

--- Recursion/nQueens.cpp
+++ Recursion/nQueens.cpp
@@ -1,34 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-   bool isSafe(vector<string>&board,int row , int col, int n){
-        //horizontal
-        for(int j = 0 ; j<n;j++){
-            if(board[row][j] == 'Q'){
-                return false;
-            }
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        // horizontal
+        for (int j = 0; j < n; j++) {
+            if (board[row][j] == 'Q') return false;
         }
 
-        //vertical
-        for(int i = 0 ; i<n;i++){
-            if (board[i][col] == 'Q'){
-                return false;
-            }
+        // vertical
+        for (int i = 0; i < n; i++) {
+            if (board[i][col] == 'Q') return false;
         }
-        //left digonal
-        for(int i = row , j = col ; i>=0 && j>=0; i--,j--){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
+
+        // top-left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
         }
-        for(int i = row , j = col ; i>=0 && j>=0; i--,j++){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
+
+        // top-right diagonal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
         }
+
         return true;
     }
 
-    
     void nQueens(vector<string>& board, int row, int n, vector<vector<string>>& ans) {
         if (row == n) {
             ans.push_back(board);
@@ -51,3 +50,18 @@ public:
         return ans;
     }
 };
+
+int main() {
+    int n = 4;
+    Solution obj;
+    vector<vector<string>> result = obj.solveNQueens(n);
+
+    for (auto& solution : result) {
+        for (string row : solution) {
+            cout << row << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
