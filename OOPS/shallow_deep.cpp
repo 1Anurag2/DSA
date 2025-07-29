c686c2b0acc4210229a6cff13eb7bab6168ad34a
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-30 00:13:39 +0530

    shallow_deep

---
+++ OOPS/shallow_deep.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        double* cgpaptr;   // Pointer to dynamically allocated memory for CGPA
+        
+        Student(string name, double cgpa){
+            this->name = name;
+            cgpaptr = new double;  // Dynamically allocate memory for CGPA
+            *cgpaptr = cgpa;      // Assign value to the dynamically allocated memory
+        }
+       
+        // Student(const Student &obj){
+        //     cout<<"Copy constructor called"<<endl;
+        //     this->name = obj.name;
+        //     this->cgpaptr = obj.cgpaptr;   // Shallow copy of the pointer
+        // }
+
+        Student(const Student &obj){
+            this->name = obj.name;
+            cgpaptr = new double;
+            *cgpaptr = *obj.cgpaptr; // Deep copy of the pointer
+        }
+
+        void getinfo(){
+            cout<<"Name: "<<name<<", CGPA: "<<*cgpaptr<<endl;
+        }
+};
+int main(){
+    Student s1("Anurag", 8.9);
+    s1.getinfo();
+    Student s2(s1); 
+    *(s2.cgpaptr) = 9.5;
+    s1.getinfo(); 
+    s2.getinfo(); // This will show that s1's CGPA is unchanged due to deep copy
+    return 0;
+}
