276cd59f9f1818e1b95c062c469ce719dba267ae
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-29 23:43:49 +0530

    Class and Object

--- OOPS/class.cpp
+++ OOPS/class.cpp
@@ -1,60 +1,55 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
-class Hero
-{
-private:
-    int health;
+class Teacher{
+    private:
+        double salary;
+    public:
+        string name;
+        int age;
+        string subject;
+        string dept;
 
-public:
+        // Non Parameterized constructor 
+        Teacher(){
+            cout<<"Default constructor called"<<endl;
+            dept = "CSIT";
+        }
+        // Parameterized constructor
+        Teacher(string name, int age, string subject, double salary){
+            cout<<"Parameterized constructor called"<<endl;
+            this->name = name;
+            this->age = age;
+            this->subject = subject;
+            this->salary = salary;
+        }
+        // Copy constructor
+        Teacher(const Teacher &obj){
+            cout<<"Copy constructor called"<<endl;
+            this->name = obj.name;
+            this->age = obj.age;  
+            this->subject = obj.subject;
+            this->salary = obj.salary;
+        }
 
-    Hero(){                // default costructor 
-        cout<<"this ->"<<this<<endl;
-    }
-
-    Hero(int Health){     //paramerised constructor
-        this->health = Health;
-    }
-
-    void print(){
-        cout<<"health of anurag is : "<<health;
-    }
-    char name[10];
-    char level;
-    int gethealth(){
-        return health;
-    }
-    void sethealth(int a){
-        health = a;
-    }
+        //setter
+        void setSalary(double salary){
+            this->salary = salary;
+        }
+        //getter
+        double getSalary(){
+            return salary;
+        }
 };
-int main()
-{
+int main(){
+    Teacher t1;  // Default constructor
+    t1.name = "Alice";
+    t1.age = 25;
+    t1.subject = "Maths";
+    t1.setSalary(50000);
+    cout<<t1.name<<" "<<t1.age<<" "<<t1.subject<<" "<<t1.getSalary()<<" "<<endl;
 
-    Hero Anurag(100);
-    Anurag.print();
-
-
-
-
-
-
-
-    //static
-    // Hero h1;
-    // cout<<"Address of object : "<<&h1<<endl;
-    
-    // Hero *b = new Hero;
-
-    // dynamic allocation
-    // Hero *b = new Hero;
-    // cout<<"level is : "<<(*b).level<<endl;
-    // cout<<"Health is : "<<(*b).gethealth()<<endl;
-
-    // cout<<"level is : "<<b->level<<endl;
-    // cout<<"Health is : "<<b->gethealth()<<en dl;
-
-    // h1.sethealth(30);
-    // cout << "Health of ramesh : " << h1.gethealth() << endl;
-    // cout << "size : " << sizeof(h1);
+    Teacher t2(t1); // Copy constructor
+    cout<<t2.name<<" "<<t2.age<<" "<<t2.subject<<" "<<endl;
     return 0;
 }
